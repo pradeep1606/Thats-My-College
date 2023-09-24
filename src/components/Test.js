8b4738a1f66bab7c6f04158d100316c5c9f5d239
Pradeep <pradeepmaurya1606@gmail.com>
2023-09-24 11:31:52 +0530

    redux test working

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,29 +1,22 @@
 'use client'
-
+import { fetchColleges } from '@/store/slices/FilterCollege';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchColleges } from '@/store/slices/FilterCollege';
-import { fetchApi } from '@/store/slices/FilterCollege';
 
-const Test=()=> {
-  const dispatch = useDispatch();
-  const colleges = useSelector(state => state.data);
+const Test = () => {
+    const dispatch = useDispatch();
+    const { colleges, loading, error } = useSelector((state) => state.allCollege);
+   
+    console.log(colleges.data);
+  
+    useEffect(() => {
+      dispatch(fetchColleges());
+    }, [dispatch]);
 
-  console.log("logging data", fetchApi);
-
-  useEffect(() => {
-      dispatch(fetchColleges(fetchApi));
-  }, [dispatch]);
-
-// const handleClick=()=>{
-//     dispatch(fetchColleges());
-// }
 
   return (
     <div>
-        hello
-        {/* <button onClick={handleClick}>click here </button> */}
-    
+      hello
     </div>
   );
 }
--- src/store/slices/FilterCollege.js
+++ src/store/slices/FilterCollege.js
@@ -3,25 +3,37 @@ import axios from "axios";
 
 const Api = "https://thats-my-college.onrender.com/api/courses/get-all/college-details";
 
-export const fetchApi = createAsyncThunk("FilterCollege/fetchColleges", async () => {
-  try {
-    const response = await axios.get(Api);
-    return response.data; 
-  } catch (error) {
-    throw error;
-  }
+export const fetchColleges = createAsyncThunk("FilterCollege/fetchColleges", async () => {
+    try {
+        const response = await axios.get(Api);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
 });
+
 
 export const FilterCollege = createSlice({
-  name: "FilterCollege",
-  initialState: [],
-  reducers: {
-    fetchColleges:(state, action)=>{  
-        // console.log("clicked");
-        state = action.payload;
-    }
-  }
+    name: 'filterCollege',
+    initialState: {
+        colleges: [],
+        loading: false,
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchColleges.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchColleges.fulfilled, (state, action) => {
+                state.loading = false;
+                state.colleges = action.payload;
+            })
+            .addCase(fetchColleges.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            });
+    },
 });
-
-export const {fetchColleges} = FilterCollege.actions
-
