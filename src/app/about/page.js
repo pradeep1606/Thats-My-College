d058f9c133080515219646ec0a7c8082a24838c3
yash yadav <132824732+yaduwanshiyash@users.noreply.github.com>
2024-02-13 00:32:31 +0530

    team details updates

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -15,12 +15,12 @@ const About = () => {
           <p className=' text-xl sm:text-xl mt-5'>Welcome to That&apos;s my college, your one-stop destination for comprehensive information on colleges and universities, designed to simplify your admission process and help you make informed decisions about your academic future.</p>
         </div>
         <div className='w-full h-fit flex flex-col sm:flex-row items-center justify-center gap-5 sm:px-28 sm:py-10'>
-          <div className=' bg-[#FFFFFF]  sm:w-2/4 sm:h-[50vh]  w-11/12 flex items-center justify-center flex-col text-center gap-5 [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] px-10 py-5'>
+          <div className=' bg-[#FFFFFF]  sm:w-2/4 sm:h-[52vh]  w-11/12 flex items-center justify-center flex-col text-center gap-5 [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] px-10 py-5'>
           <Image src="/images/icons8-mission-64.png" className="h-17 mt-2 w-21 -mr-2" width={70} height={60} priority={true} alt="TMC" />
             <h2 className=' text-2xl text-zinc-800'>Our Mission</h2>
             <p className=' text-xl'>Support students in their studies by providing a seamless and accessible platform that streamlines the university search and admissions process. Our mission is to create educational opportunities, close knowledge gaps, and develop a community where students can thrive academically and professionally.</p>
           </div>
-          <div className=' bg-[#FFFFFF]  sm:w-2/4 sm:h-[50vh] w-11/12 flex items-center justify-center flex-col text-center gap-5 [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] px-10 py-5'>
+          <div className=' bg-[#FFFFFF]  sm:w-2/4 sm:h-[52vh] w-11/12 flex overflow-y-auto [&::-webkit-scrollbar]:hidden items-center justify-center flex-col text-center gap-5 [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] px-10 py-5'>
           <Image src="/images/icons8-people-40.png" className="h-17 mt-2 w-21 -mr-2" width={70} height={60} priority={true} alt="TMC" />
             <h2 className=' text-2xl text-zinc-800'>Our Mission</h2>
             <p className=' text-xl'>To be the leading and most trusted online platform by providing a comprehensive and innovative platform that not only accelerates the access process for students seeking higher education but also promotes technical education. We envision a future where all students can easily find and connect with schools, access quality resources, and step into a successful education and career path.</p>
@@ -33,7 +33,7 @@ const About = () => {
           <div className=' user h-[fit-content] w-full flex flex-col sm:grid sm:grid-cols-4 sm:px-[10vw] pb-10 pt-8 sm:flex-row items-center justify-center gap-[5vw]'>
           <div className='card relative h-fit w-[75vw] sm:h-[50vh] sm:w-[18vw] bg-[lavender] pb-2 sm:pb-0 rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] [transition:all_ease_0.5s]'>
               <div className='relative h-[fit-content] w-[80vw] flex sm:w-[18vw]'>
-              <div className=' photo h-[28vh] sm:h-[30vh] w-[50vw] sm:w-[16vw]  relative sm:ml-0 ml-5 top-[3vh] bg-[url("/team/yash.jpg")] bg-cover rounded-[10px] bg-center '></div>
+              <div className=' photo h-[28vh] sm:h-[30vh] w-[50vw] sm:w-[16vw]  relative sm:ml-0 ml-5 top-[3vh] bg-[url("/team/yash_t.jpg")] bg-cover rounded-[10px] bg-center '></div>
               <div className='social sm:opacity-0 opacity-1 h-[35vh] w-[15vw] ml-[5vw] sm:w-[5vw] sm:ml-[0vw] text-[rgba(45,94,176,1)] [transition:all_ease_0.5s]'>
               <div className="flex flex-col gap-3 mt-8">
                 <a href="mailto:Yashrajthakur280@gmail.com" className="w-10 text-lg  h-10 bg-white bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-colors duration-300 hover:text-gray-800 hover:bg-white">
@@ -47,7 +47,7 @@ const About = () => {
               </div>
               <div className=' name text-[#224074] relative [transition:all_ease_0.5s]'>
                 <h2 className=' text-xl'>Yash Thakur</h2>
-                <h2 className=' text-xl'>CTO/Co-founder</h2>
+                <h2 className=' text-xl'>CEO/Co-founder</h2>
               </div>
             </div>
             <div className='card relative h-fit w-[75vw] sm:h-[50vh] sm:w-[18vw] bg-[lavender] pb-2 sm:pb-0 rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] [transition:all_ease_0.5s]'>
@@ -130,6 +130,27 @@ const About = () => {
             </div>
             <div className='card relative h-fit w-[75vw] sm:h-[50vh] sm:w-[18vw] bg-[lavender] pb-2 sm:pb-0 rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] [transition:all_ease_0.5s]'>
               <div className='relative h-[fit-content] w-[80vw] flex sm:w-[18vw]'>
+              <div className=' photo h-[28vh] sm:h-[30vh] w-[50vw] sm:w-[16vw]  relative sm:ml-0 ml-5 top-[3vh] bg-[url("/team/ujjawalf.jpg")] bg-cover rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] bg-center '></div>
+              <div className='social sm:opacity-0 opacity-1 h-[35vh] w-[15vw] ml-[5vw] sm:w-[5vw] sm:ml-[0vw] text-[rgba(45,94,176,1)] [transition:all_ease_0.5s]'>
+              <div className="flex flex-col gap-3 mt-8">
+                <a href="https://www.instagram.com/ujjwal8478?igsh=MzRlODBiNWFlZA==" className="w-10 text-lg  h-10 bg-white bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-colors duration-300 hover:text-gray-800 hover:bg-white">
+                  <FaInstagram />
+                </a>
+                <a href="https://www.linkedin.com/in/yash-yaduwanshi-210864279" className="w-10 text-lg  h-10 bg-white bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-colors duration-300 hover:text-gray-800 hover:bg-white">
+                  <FaLinkedinIn />
+                </a>
+              </div>
+              </div>
+              </div>
+              <div className=' name text-[#224074] relative [transition:all_ease_0.5s]'>
+                <h2 className=' text-xl'>Ujjawal Tiwari</h2>
+                <h2 className=' text-xl'>Co-founder ,COO</h2>
+                <h2 className=' text-xl'>Managing Director</h2>
+                {/* <h2 className=' text-xl'> (CMO)</h2> */}
+              </div>
+            </div>
+            <div className='card relative h-fit w-[75vw] sm:h-[50vh] sm:w-[18vw] bg-[lavender] pb-2 sm:pb-0 rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] [transition:all_ease_0.5s]'>
+              <div className='relative h-[fit-content] w-[80vw] flex sm:w-[18vw]'>
               <div className=' photo h-[28vh] sm:h-[30vh] w-[50vw] sm:w-[16vw]  relative sm:ml-0 ml-5 top-[3vh] bg-[url("/team/naman.jpg")] bg-cover rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] bg-center '></div>
               <div className='social sm:opacity-0 opacity-1 h-[35vh] w-[15vw] ml-[5vw] sm:w-[5vw] sm:ml-[0vw] text-[rgba(45,94,176,1)] [transition:all_ease_0.5s]'>
               <div className="flex flex-col gap-3 mt-8">
@@ -151,26 +172,6 @@ const About = () => {
               <div className=' name text-[#224074] relative [transition:all_ease_0.5s]'>
                 <h2 className=' text-xl'>Naman Raghuwanshi</h2>
                 <h2 className=' text-xl'>Social Media Manager</h2>
-              </div>
-            </div>
-            <div className='card relative h-fit w-[75vw] sm:h-[50vh] sm:w-[18vw] bg-[lavender] pb-2 sm:pb-0 rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] [transition:all_ease_0.5s]'>
-              <div className='relative h-[fit-content] w-[80vw] flex sm:w-[18vw]'>
-              <div className=' photo h-[28vh] sm:h-[30vh] w-[50vw] sm:w-[16vw]  relative sm:ml-0 ml-5 top-[3vh] bg-[url("/team/ujjawalf.jpg")] bg-cover rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] bg-center '></div>
-              <div className='social sm:opacity-0 opacity-1 h-[35vh] w-[15vw] ml-[5vw] sm:w-[5vw] sm:ml-[0vw] text-[rgba(45,94,176,1)] [transition:all_ease_0.5s]'>
-              <div className="flex flex-col gap-3 mt-8">
-                <a href="https://www.instagram.com/ujjwal8478?igsh=MzRlODBiNWFlZA==" className="w-10 text-lg  h-10 bg-white bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-colors duration-300 hover:text-gray-800 hover:bg-white">
-                  <FaInstagram />
-                </a>
-                <a href="https://www.linkedin.com/in/yash-yaduwanshi-210864279" className="w-10 text-lg  h-10 bg-white bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-colors duration-300 hover:text-gray-800 hover:bg-white">
-                  <FaLinkedinIn />
-                </a>
-              </div>
-              </div>
-              </div>
-              <div className=' name text-[#224074] relative [transition:all_ease_0.5s]'>
-                <h2 className=' text-xl'>Ujjawal Tiwari</h2>
-                <h2 className=' text-xl'>Chief Marketing Officer</h2>
-                <h2 className=' text-xl'> (CMO)</h2>
               </div>
             </div>
             <div className='card relative h-fit w-[75vw] sm:h-[50vh] sm:w-[18vw] bg-[lavender] pb-2 sm:pb-0 rounded-[10px] [box-shadow:0px_15px_30px_5px_rgba(0,0,0,0.06)] [transition:all_ease_0.5s]'>
