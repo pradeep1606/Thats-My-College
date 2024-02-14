c0b2b47e1810642d13528059ce0664562c7d50c5
Pradeep Maurya <pradeepmaurya1606@gmail.com>
2024-02-14 20:20:10 +0530

    Revert "filter button close & fux some issues"

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -151,16 +151,16 @@ const Contact = () => {
 
             <h2 className="text-xl font-semibold mt-8 mb-4 text-slate-700">Follow us on </h2>
             <div className="flex">
-              <a href="#" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white" target='_blank'>
+              <a href="#" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white">
                 <FaFacebookF />
               </a>
-              <a href="https://x.com/thatsmycollege?s=08" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white" target='_blank'>
+              <a href="https://x.com/thatsmycollege?s=08" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white">
                 <FaTwitter />
               </a>
-              <a href="https://www.instagram.com/thats_mycollege?utm_source=qr&igsh=czVsdmxnejI3Z3Ro" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white" target='_blank'>
+              <a href="https://www.instagram.com/thats_mycollege?utm_source=qr&igsh=czVsdmxnejI3Z3Ro" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white">
                 <FaInstagram />
               </a>
-              <a href="https://www.linkedin.com/in/that-s-my-college-3470392ab?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white" target='_blank'>
+              <a href="https://www.linkedin.com/in/that-s-my-college-3470392ab?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="w-10 h-10 bg-blue-400 bg-opacity-20 mr-4 flex items-center justify-center rounded-full transition-all duration-300 hover:bg-gray-500 hover:text-white">
                 <FaLinkedinIn />
               </a>
             </div>
--- src/components/AppliedInternship.js
+++ src/components/AppliedInternship.js
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link';
 import React from 'react'
 import { MdDelete } from "react-icons/md";
 
@@ -7,15 +6,9 @@ const AppliedInternship = () => {
   return (
     <>
       <div className='mt-4 text-[4vw] sm:text-[1.5vw]'>
-        <h2 className='capitalize text-xl md:font-semibold mt-2'>applied internship</h2>
-        <div className='[&::-webkit-scrollbar]:hidden h-[40vh] md:h-[40vh] w-[85vw] flex items-center overflow-x-scroll overflow-y-hidden whitespace-nowrap p-2 gap-3'>
-          <div className='-mt-24 px-10'>
-            <p className='text-base'>Internship data not found</p>
-            {/* <p className='text-base italic'>
-              click <Link className='text-blue-700' href={{ pathname: '/colleges', query: { name: 'Engineering' }, }}>here</Link> to apply colleges
-            </p> */}
-          </div>
-          {/* <div className='flex gap-2 flex-col border-2 border-solid border-[rgb(241,238,238)] rounded-lg shadow-md'>
+          <h2 className='capitalize text-xl md:font-semibold mt-2'>applied internship</h2>
+          <div className='[&::-webkit-scrollbar]:hidden h-[40vh] md:h-[40vh] w-[85vw] flex items-center overflow-x-scroll overflow-y-hidden whitespace-nowrap p-2 gap-3'>
+            {/* <div className='flex gap-2 flex-col border-2 border-solid border-[rgb(241,238,238)] rounded-lg shadow-md'>
               <div className='h-[25vh] w-[280px] md:w-[300px] relative bg-[lavender] flex flex-col items-center justify-center rounded-t-lg overflow-hidden'>
                 <div className='bg-cover bg-center'>
                   <Image src='/images/back-img.jpeg' width={1000} height={400} alt="" className='object-cover object-center h-full w-full filter brightness-[90%]' />
@@ -79,8 +72,8 @@ const AppliedInternship = () => {
                 <button className='text-2xl'><MdDelete /></button>
               </div>
             </div> */}
+          </div>
         </div>
-      </div>
     </>
   )
 }
